27be2b24092e1b1fc8cb812ef4be64c3566bac44
JordanLaney <jlaney4@mail.csuchico.edu>
2018-07-23 14:34:11 -0700

    Http Get

---
+++ list-files-main.js
@@ -0,0 +1,11 @@
+let dirPath = process.argv[2];
+let fileExt = process.argv[3];
+
+let listModule = require('./module-list-files-ext.js');
+function listFiles(err, list) {
+	if(err) throw err;
+	for(let i = 0; i < list.length; i++) {
+		console.log(list[i]);
+	}
+}
+listModule(dirPath, fileExt, listFiles);
---
+++ module-list-files-ext.js
@@ -0,0 +1,14 @@
+let fs = require('fs');
+
+module.exports = function(dirPath, fileExt, callback) {
+	fs.readdir(dirPath,function(err, list) {
+		if(err) return callback(err);
+			let subList = [];
+			for(let i = 0; i < list.length; i++) {
+				if(fileExt === list[i].split('.').pop() && list[i].includes('.')) {
+					subList.push(list[i]);
+				}
+			}
+			callback(null, subList);
+	});
+}
