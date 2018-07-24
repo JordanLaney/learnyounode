f169b4786be055a4f0fd04f163e8af3d32c9acf4
JordanLaney <jlaney4@mail.csuchico.edu>
2018-07-24 09:28:17 -0700

    9/13

---
+++ buffer-list-get-request.js
@@ -0,0 +1,10 @@
+let http = require('http')
+let bl = require('bl');
+
+http.get(process.argv[2], response => {
+	response.pipe(bl(function(err, data) {
+		if(err) return console.error(err);
+		console.log(data.toString().length);
+		console.log(data.toString());
+	}));
+});
